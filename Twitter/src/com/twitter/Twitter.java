2a7850f2823e142467502151aff8bbe0e57f6659
mirjanastanojevic9914 <mira.95.bb@gmail.com>
2016-03-29 22:18:10 +0200

    Napisana JavaDoc dokumentacija za klasu Twitter, njene atribute i metode; generisane JavaDoc HTML strane i stavljeno da git ignorise doc folder sa JavaDoc HTML dokumentacijom.

--- Twitter/src/com/twitter/Twitter.java
+++ Twitter/src/com/twitter/Twitter.java
@@ -3,14 +3,33 @@ package com.twitter;
 import java.util.LinkedList;
 import com.twitter.poruke.TwitterPoruka;
 
+/**
+ * 
+ * Klasa Twitter sa implementiranim metodama vratiSvePoruke, unesi i vrtaiPoruke.
+ * @author Mirjana Stanojevic
+ * @version 2.0
+ *
+ */
 public class Twitter {
-
+   
+	/**
+	 * Lista twitter poruka.
+	 */
 	private LinkedList<TwitterPoruka> poruke = new LinkedList<TwitterPoruka>();
-
+    
+	/**
+	 * Metoda vraca listu svih poruka.
+	 * @return lista poruka
+	 */
 	public LinkedList<TwitterPoruka> vratiSvePoruke() {
 		return poruke;
 	}
-
+    
+	/**
+	 * Metoda koja pravi novu poruku, puni je podacima i ubacuje na kraj liste.
+	 * @param korisnik ime korisnika koji salje twitter poruku
+	 * @param poruka twitter poruka koja se salje
+	 */
 	public void unesi(String korisnik, String poruka) {
 		// Pravi se nova poruka i puni podacima.
 		TwitterPoruka tp = new TwitterPoruka();
@@ -19,7 +38,18 @@ public class Twitter {
 		// Poruka se unosi u listu na kraj
 		poruke.addLast(tp);
 	}
-
+    
+	/**
+	 * Metoda koja vraca niz poruka koje sadrze tag prosledjen kao ulazni parametar.
+	 * @param maxBroj max broj poruka koje metoda moze vratiti
+	 * @param tag tag koji se trazi u twitter porukama
+	 * @return niz poruka koje sadrze tag.
+	 * @throws java.lang.RuntimeException ako je prosledjeni tag
+	 * <ul>
+	 * <li>null</li> 
+	 * <li>prazan string.</li>
+	 * </ul>
+	 */
 	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
 		if (tag == null || tag == "")
 			throw new RuntimeException("Morate uneti tag");
